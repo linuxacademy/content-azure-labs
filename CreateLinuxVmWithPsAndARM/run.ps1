a7913eb2a43b0747151562f25a610c964d00b01d
Michael Heydt <mike@heydt.org>
2018-10-01 21:14:02 -0600

    Added CreateLinuxVmWithPsAndArm Project

---
+++ CreateLinuxVmWithPsAndARM/run.ps1
@@ -0,0 +1,2 @@
+New-AzureRmResourceGroup -Name ExampleResourceGroup -Location "South Central US"
+New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName ExampleResourceGroup -TemplateFile template.json -TemplateParameterFile parameters.json -Verbose
