acc3ec4728a8ece4e56ddecc07c076b65e810f93
Michael Heydt <mike@heydt.org>
2018-10-01 22:01:55 -0600

    Modified run script

--- CreateLinuxVmWithPsAndARM/run.ps1
+++ CreateLinuxVmWithPsAndARM/run.ps1
@@ -1,2 +1,4 @@
-New-AzureRmResourceGroup -Name ExampleResourceGroup -Location "South Central US"
-New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName ExampleResourceGroup -TemplateFile template.json -TemplateParameterFile parameters.json -Verbose
+New-AzureRmResourceGroupDeployment -Name ExampleDeployment `
+    -ResourceGroupName ReplaceMe `
+    -TemplateFile template.json `
+    -TemplateParameterFile parameters.json -Verbose
