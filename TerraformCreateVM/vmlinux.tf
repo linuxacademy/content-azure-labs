a962c4cd7c958c8a2de9f666264d84283b26d4b3
Chad Crowell <chad.crowell@linuxacademy.com>
2020-05-15 14:02:52 -0500

    changed terraform file

--- TerraformCreateVM/vmlinux.tf
+++ TerraformCreateVM/vmlinux.tf
@@ -1,99 +1,109 @@
 locals {
   resource_group_name = ""
+  location = "westus"
   public_key = ""
-  location = "southcentralus"
 }
 
-# Configure Azure Provider
+# configure azure provider
 provider "azurerm" {
     version = "=2.0.0"
     features {}
 }
 
-# Create virtual network
-resource "azurerm_virtual_network" "lab_vnet" {
-    name = "labVNET"
-    address_space = ["10.0.0.0/16"]
-    location = "local.location"
-    resource_group_name = "local.resource_group_name"
+resource "random_string" "rand" {
+ length = 24
+ special = false
+ upper = false
 }
 
-# Create subnet
-resource "azurerm_subnet" "lab_subnet" {
-    name = "labSUBNET"
-    resource_group_name = "local.resource_group_name"
-    virtual_network_name = "${azurerm_virtual_network.lab_vnet.name}"
-    address_prefix = "10.0.1.0/24"
-}
-
-# Create public IPs
-resource "azurerm_public_ip" "lab_pip" {
-    name = "labPIP"
-    location = "local.location"
-    resource_group_name = "local.resource_group_name"
-    allocation_method = "Dynamic"
-}
-
-# Create Network Security Group and rule
+# create network security group
 resource "azurerm_network_security_group" "lab_nsg" {
-    name = "labNSG"
-    location = "local.location"
-    resource_group_name = "local.resource_group_name"
-    security_rule {
-        name = "SSH"
-        priority = 1001
-        direction = "Inbound"
-        access = "Allow"
-        protocol = "Tcp"
-        source_port_range = "*"
-        destination_port_range = "22"
-        source_address_prefix = "*"
-        destination_address_prefix = "*"
-    }
+  name                = random_string.rand.result
+  location            = local.location
+  resource_group_name = local.resource_group_name
 }
 
-# Create network interface
+# create network security group rule
+resource "azurerm_network_security_rule" "example" {
+  name                        = "SSH"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = local.resource_group_name
+  network_security_group_name = azurerm_network_security_group.lab_nsg.name
+}
+
+# create virtual network
+resource "azurerm_virtual_network" "lab_vnet" {
+  name                = random_string.rand.result
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+}
+
+# create subnet
+resource "azurerm_subnet" "lab_subnet" {
+  name                 = "labSUBNET"
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = azurerm_virtual_network.lab_vnet.name
+  address_prefix       = "10.0.2.0/24"
+}
+
+# create public ip
+resource "azurerm_public_ip" "lab_pip" {
+  name                = "PublicIp1"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  allocation_method   = "Dynamic"
+}
+
+# create network interface
 resource "azurerm_network_interface" "lab_nic" {
-    name = "labNIC"
-    location = "local.location"
-    resource_group_name = "local.resource_group_name"
-    
-    ip_configuration {
-        name = "labNicConfiguration"
-        subnet_id = "${azurerm_subnet.lab_subnet.id}"
-        private_ip_address_allocation = "dynamic"
-        public_ip_address_id = "${azurerm_public_ip.lab_pip.id}"
-    }
+  name                = "labNIC"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.lab_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
-# Create virtual machine
 resource "azurerm_virtual_machine" "lab_vm" {
-    name = "labVM"
-    location = "local.location"
-    resource_group_name = "local.resource_group_name"
-    network_interface_ids = ["${azurerm_network_interface.lab_nic.id}"]
-    vm_size = "Standard_B1ms"
-    storage_os_disk {
-        name = "labVMOSDISK"
-        caching = "ReadWrite"
-        create_option = "FromImage"
-        managed_disk_type = "Premium_LRS"
+  name                  = "labVM"
+  location              = local.location
+  resource_group_name   = local.resource_group_name
+  network_interface_ids = [azurerm_network_interface.lab_nic.id]
+  vm_size               = "Standard_B1ms"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      path = "/home/testadmin/.ssh/authorized_keys"
+      key_data = local.public_key
     }
-    storage_image_reference {
-        publisher = "Canonical"
-        offer = "UbuntuServer"
-        sku = "16.04.0-LTS"
-        version = "latest"
-    }
-    os_profile {
-        computer_name = "labvm"
-        admin_username = "labvmadmin"
-    }
-    os_profile_linux_config {
-        disable_password_authentication = true
-        ssh_keys {
-            path = "/home/labvmadmin/.ssh/authorized_keys"
-            key_data = "local.public_key"
-        }
-    }
+  }
 }
